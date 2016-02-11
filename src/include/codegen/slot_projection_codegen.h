18cfe8efa16c16770f496b114360286b35a62aa2
Foyzur Rahman <foyzur.rahman@gopivotal.com>
2016-02-11 11:42:20 -0800

    Wrapper for Toy example for GPDB Balerion integration

---
+++ src/include/codegen/slot_projection_codegen.h
@@ -0,0 +1,27 @@
+/*
+ * slot_projection_codegen.h
+ *
+ *  Created on: Feb 5, 2016
+ *      Author: rahmaf2
+ */
+
+#ifndef SRC_BACKEND_CODEGEN_SLOT_PROJECTION_CODEGEN_H_
+#define SRC_BACKEND_CODEGEN_SLOT_PROJECTION_CODEGEN_H_
+
+#include "balerion/clang_compiler.h"
+#include "balerion/code_generator.h"
+
+class SlotProjectionCodeGen {
+public:
+	SlotProjectionCodeGen();
+	virtual ~SlotProjectionCodeGen() {};
+	void GenerateDummyIRModule();
+	void PrepareForExecution();
+	//int (*GetDummyIRModule(int)) ();
+	auto GetDummyIRModule() -> int(*) (int);
+
+private:
+	std::unique_ptr<balerion::CodeGenerator> code_generator_;
+};
+
+#endif /* SRC_BACKEND_CODEGEN_SLOT_PROJECTION_CODEGEN_H_ */
