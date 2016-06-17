62cf67bd068ece745393c9fee2557b3c6414e23c
Foyzur Rahman <frahman@gmail.com>
2016-06-16 22:30:21 -0700

    template specialize for arithmetic operation
    Signed-off-by: Karthikeyan Jambu Rajaraman <karthi.jrk@gmail.com>

--- src/backend/codegen/include/codegen/pg_int_expr_generator.h
+++ src/backend/codegen/include/codegen/pg_int_expr_generator.h
@@ -26,34 +26,116 @@ namespace gpcodegen {
 /** \addtogroup gpcodegen
  *  @{
  */
-
+template <typename rtype, typename Arg0, typename Arg1>
 class PGIntExprGenerator {
  public:
-  template <typename rtype, typename Arg0, typename Arg1>
-  static bool MultWithOverflow(gpcodegen::CodegenUtils* codegen_utils,
+  static bool MulWithOverflow(gpcodegen::CodegenUtils* codegen_utils,
                                llvm::Function* llvm_main_func,
                                llvm::BasicBlock* llvm_error_block,
                                std::vector<llvm::Value*>& llvm_args,
                                llvm::Value* & llvm_out_value);
+
+  static bool AddWithOverflow(gpcodegen::CodegenUtils* codegen_utils,
+                              llvm::Function* llvm_main_func,
+                              llvm::BasicBlock* llvm_error_block,
+                              std::vector<llvm::Value*>& llvm_args,
+                              llvm::Value* & llvm_out_value);
+
+  static bool SubWithOverflow(gpcodegen::CodegenUtils* codegen_utils,
+                                llvm::Function* llvm_main_func,
+                                llvm::BasicBlock* llvm_error_block,
+                                std::vector<llvm::Value*>& llvm_args,
+                                llvm::Value* & llvm_out_value);
 };
 
 template <typename rtype, typename Arg0, typename Arg1>
-bool PGIntExprGenerator::MultWithOverflow(gpcodegen::CodegenUtils* codegen_utils,
+bool PGIntExprGenerator<rtype, Arg0, Arg1>::MulWithOverflow(gpcodegen::CodegenUtils* codegen_utils,
                                           llvm::Function* llvm_main_func,
                                           llvm::BasicBlock* llvm_error_block,
                                           std::vector<llvm::Value*>& llvm_args,
                                           llvm::Value* & llvm_out_value) {
   // Assumed caller checked vector size and nullptr for codegen_utils
-
   llvm::BasicBlock* llvm_non_overflow_block = codegen_utils->CreateBasicBlock(
-             "non_overflow_block", llvm_main_func);
+             "mul_non_overflow_block", llvm_main_func);
   llvm::BasicBlock* llvm_overflow_block = codegen_utils->CreateBasicBlock(
-             "overflow_block", llvm_main_func);
+             "mul_overflow_block", llvm_main_func);
 
-  llvm::Value* llvm_arg0 = codegen_utils->CreateCast<rtype>(llvm_args[0]);
-  llvm::Value* llvm_arg1 = codegen_utils->CreateCast<rtype>(llvm_args[1]);
-  llvm::Value* llvm_mul_output = codegen_utils->CreateSMulOverflow(llvm_arg0,
-                                                                   llvm_arg1);
+  llvm::Value* llvm_mul_output = codegen_utils->CreateMulOverflow<rtype>(
+      llvm_args[0], llvm_args[1]);
+
+  llvm::IRBuilder<>* irb = codegen_utils->ir_builder();
+
+  llvm_out_value = irb->CreateExtractValue(llvm_mul_output, 0);
+  llvm::Value* llvm_overflow_flag = irb->CreateExtractValue(llvm_mul_output, 1);
+
+  irb->CreateCondBr(
+      irb->CreateICmpEQ(llvm_overflow_flag,
+                        codegen_utils->GetConstant<bool>(true)),
+                        llvm_overflow_block,
+                        llvm_non_overflow_block );
+
+  irb->SetInsertPoint(llvm_overflow_block);
+  // TODO : krajaraman Elog::ERROR after ElogWrapper integrad.
+  irb->CreateBr(llvm_error_block);
+
+  irb->SetInsertPoint(llvm_non_overflow_block);
+
+  return true;
+}
+
+template <typename rtype, typename Arg0, typename Arg1>
+bool PGIntExprGenerator<rtype, Arg0, Arg1>::AddWithOverflow(
+    gpcodegen::CodegenUtils* codegen_utils,
+    llvm::Function* llvm_main_func,
+    llvm::BasicBlock* llvm_error_block,
+    std::vector<llvm::Value*>& llvm_args,
+    llvm::Value* & llvm_out_value) {
+
+  // Assumed caller checked vector size and nullptr for codegen_utils
+  llvm::BasicBlock* llvm_non_overflow_block = codegen_utils->CreateBasicBlock(
+      "add_non_overflow_block", llvm_main_func);
+  llvm::BasicBlock* llvm_overflow_block = codegen_utils->CreateBasicBlock(
+      "add_overflow_block", llvm_main_func);
+
+  llvm::Value* llvm_mul_output = codegen_utils->CreateAddOverflow<rtype>(
+      llvm_args[0], llvm_args[1]);
+
+  llvm::IRBuilder<>* irb = codegen_utils->ir_builder();
+
+  llvm_out_value = irb->CreateExtractValue(llvm_mul_output, 0);
+  llvm::Value* llvm_overflow_flag = irb->CreateExtractValue(llvm_mul_output, 1);
+
+  irb->CreateCondBr(
+      irb->CreateICmpEQ(llvm_overflow_flag,
+                        codegen_utils->GetConstant<bool>(true)),
+                        llvm_overflow_block,
+                        llvm_non_overflow_block );
+
+  irb->SetInsertPoint(llvm_overflow_block);
+  // TODO : krajaraman Elog::ERROR after ElogWrapper integrad.
+  irb->CreateBr(llvm_error_block);
+
+  irb->SetInsertPoint(llvm_non_overflow_block);
+
+  return true;
+}
+
+template <typename rtype, typename Arg0, typename Arg1>
+bool PGIntExprGenerator<rtype, Arg0, Arg1>::SubWithOverflow(
+    gpcodegen::CodegenUtils* codegen_utils,
+    llvm::Function* llvm_main_func,
+    llvm::BasicBlock* llvm_error_block,
+    std::vector<llvm::Value*>& llvm_args,
+    llvm::Value* & llvm_out_value) {
+
+  // Assumed caller checked vector size and nullptr for codegen_utils
+  llvm::BasicBlock* llvm_non_overflow_block = codegen_utils->CreateBasicBlock(
+      "sub_non_overflow_block", llvm_main_func);
+  llvm::BasicBlock* llvm_overflow_block = codegen_utils->CreateBasicBlock(
+      "sub_overflow_block", llvm_main_func);
+
+  llvm::Value* llvm_mul_output = codegen_utils->CreateSubOverflow<rtype>(
+      llvm_args[0], llvm_args[1]);
 
   llvm::IRBuilder<>* irb = codegen_utils->ir_builder();
 
