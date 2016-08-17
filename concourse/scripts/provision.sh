e89a4323d3f88029f6ca1f6d46c87722f480a9e7
Shreedhar Hardikar <shardikar@pivotal.io>
2016-08-16 18:35:14 -0700

    Provision step 1

---
+++ concourse/scripts/provision.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo ${AWS_SECRET_KEY}
+echo ${AWS_ACCESS_KEY}
+echo ${AWS_SECRET_KEY}
+echo ${AWS_ACCESS_KEY}
+echo ${AWS_KEYPAIR}
+echo ${AMI}
+echo ${INSTANCE_TYPE}
+echo ${VPC_ID}
+echo ${SUBNET_ID}
+echo ${SSH_USER}
+echo ${IMAGE_NAME}
+echo ${TENANCY}
