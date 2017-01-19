07239e25ed123ee9512139f71ffc5bef6cdd9e81
Karthikeyan Jambu Rajaraman <karthi.jrk@gmail.com>
2017-01-19 14:55:28 -0800

    Adding comments.

--- src/include/fstream/gfile.h
+++ src/include/fstream/gfile.h
@@ -31,7 +31,13 @@ typedef char bool_t;
 #endif
 #endif
 
+/* gfile_t is the private struct and it is defined in gfile.c */
+typedef struct gfile_t gfile_t;
 struct gpfxdist_t;
+
+/* Typedef for the gfile_t's allocator */
+typedef void*(*GFileAlloca)(size_t size);
+typedef void(*GFileFree)(void*);
 
 typedef enum Compression_type
 {
@@ -39,13 +45,6 @@ typedef enum Compression_type
 	GZ_COMPRESSION,
 	BZ_COMPRESSION
 } compression_type;
-
-typedef enum FileAccessType
-{
-	Invalid = 0,
-	CFileObj,
-	PostgresFileObj
-} FileAccessType;
 
 typedef enum GFileErrorCode
 {
@@ -60,6 +59,21 @@ typedef enum GFileErrorCode
 	GF_OutOfMemory = -8
 } GFileErrorCode;
 
+typedef enum FileAccessType
+{
+	Invalid = 0,
+	CFileObj,
+	PostgresFileObj
+} FileAccessType;
+
+/*
+ * FileAccessInterface acts as an interface(superclass) to access the
+ * file for reading / writing / closing the file.
+ * It can be of type
+ *  CFileObj - this uses low-level file descriptor.
+ *  PostgresFileObj - this uses Postgres `File` data structure. For e.g.
+ *  BFZ uses Postgres `File` data structure.
+ */
 typedef struct FileAccessInterface
 {
 	FileAccessType ftype;
@@ -67,10 +81,6 @@ typedef struct FileAccessInterface
 	ssize_t (*write_file)(struct FileAccessInterface *fileaccess, void *ptr, size_t size);
 	void (*close_file)(struct FileAccessInterface *fileaccess);
 } FileAccessInterface;
-
-typedef struct gfile_t gfile_t;
-typedef void*(*GFileAlloca)(size_t size);
-typedef void(*GFileFree)(void*);
 
 /*
  * MPP-13817 (support opening files without O_SYNC)
@@ -80,19 +90,60 @@ int gfile_open_flags(int writing, int usesync);
 #define GFILE_OPEN_FOR_WRITE_NOSYNC 1
 #define GFILE_OPEN_FOR_WRITE_SYNC   2
 
+/*
+ * gfile_t public api
+ */
+
+/*
+ * This create new gfile_t using given allocator. gfile_t will use the same
+ * allocator for any future memory allocation.
+ */
 gfile_t* gfile_create(compression_type compression, bool_t is_write, GFileAlloca gfile_alloca, GFileFree gfile_free);
+
+/*
+ * Delete the gfile_t with its own allocator.
+ */
 void gfile_destroy(gfile_t* fd);
+
+/*
+ * Open the file. This API uses `CFileObj` FileAccessInterface type.
+ * In other words, this would use low-level file descriptor for file access.
+ */
 int gfile_open(gfile_t* fd, const char* fpath, int flags, int* response_code, const char** response_string, struct gpfxdist_t* transform);
+
+/*
+ * Close the file and deletes the FileAccessInterface using gfile_t's allocator
+ */
 void gfile_close(gfile_t*fd);
+
 off_t gfile_get_compressed_size(gfile_t*fd);
 off_t gfile_get_compressed_position(gfile_t*fd);
 bool_t gfile_is_win_pipe(gfile_t* fd);
+/*
+ * Read gfile. If any error happens, it return -1 and also set
+ * the error code in gfile.
+ */
 ssize_t gfile_read(gfile_t* fd, void* ptr, size_t len); /* gfile_read reads as much as it can--short read indicates error. */
+
+/*
+ * Write gfile. If any error happens, it return -1 and also set
+ * the error code in gfile.
+ */
 ssize_t gfile_write(gfile_t* fd, void* ptr, size_t len);
 void gfile_printf_then_putc_newline(const char*format,...) __attribute__ ((__format__ (__printf__, 1, 0)));
+/*
+ * This will setup gfile_t with the given file (FileAccessInterface) and initialize
+ * zlib data structure.
+ */
 int gz_file_open(gfile_t *fd, FileAccessInterface* file_access);
+
 GFileErrorCode gfile_error_code(gfile_t *fd);
 void gfile_reset_error(gfile_t *fd);
 bool_t gfile_has_error(gfile_t *fd);
+
+/*
+ * Return any error message in detail if available.
+ * For e.g. zlib will have a more detailed error message.
+ */
 char* gfile_error_detail(gfile_t *fd);
 #endif
